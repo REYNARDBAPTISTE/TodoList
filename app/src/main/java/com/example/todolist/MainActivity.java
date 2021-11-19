53d3fc6d407a24f2854b64405b6a56aee4afcb13
reynard <baptistereynard22@outlook.fr>
2021-11-19 11:44:20 +0100

    C'est l'heure zebi pas fini, page d'accueil amélioré

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Creation
+
+        Button btnInscription = (Button) findViewById(R.id.btnInscription);
+        Button btnConnexion = (Button) findViewById(R.id.btnConnexion);
+        Button btnMdpOublie = (Button) findViewById(R.id.btnMdpOublie);
+
+        btnInscription.setBackgroundColor(Color.GRAY);
+        btnConnexion.setBackgroundColor(Color.BLUE);
+        btnMdpOublie.setBackgroundColor(Color.WHITE);
+        btnMdpOublie.setTextColor(Color.BLACK);
+        btnMdpOublie.setOutlineSpotShadowColor(Color.WHITE);
     }
 }
